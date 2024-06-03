b9036af5097468ce1c8e01957891bb0cbeb3cb4f
VenciMarev1 <168589370+VenciMarev1@users.noreply.github.com>
2024-06-03 15:21:26 +0300

    Class grade and method get grade

--- random project/Program.cs
+++ random project/Program.cs
@@ -1,10 +1,41 @@
 ﻿namespace random_project;
 
+class Grade
+{
+    public int _grade { set; get; }
+    public string _class { set; get; }
+
+    public Grade(int Grade, string Class)
+    {
+        this._class = Class;
+        this._grade = Grade;
+    }
+
+
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+    }
+
+    public Grade[] GetGradeForEveryClass(Grade[] _grade)
+    {
+        Console.WriteLine("Enter the class and the grade for the class: ");
+        for(int i = 0; i < _grade.Length; i ++)
+        {
+            Console.Write("class" + (i + 1) + ": ");
+            _grade[i]._class = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("grade: ");
+            _grade[i]._grade = int.Parse(Console.ReadLine());
+        }
+
+
+
+        return new Grade[3];
     }
 }
 
